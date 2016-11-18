b0bf9d000d9efcd98ae870c179f954db8eb40f4b
Henk Bierlee <bierlee.henk@gmail.com>
2016-11-18 21:04:47 +0100

    Connect with firebase project

---
+++ app/util/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyB8zFOduddUWq8QZ7Isn8NWOlPOt_Es20w',
+  authDomain: 'digital-shoulder-tap.firebaseapp.com',
+  databaseURL: 'https://digital-shoulder-tap.firebaseio.com',
+})
+
+export default base
