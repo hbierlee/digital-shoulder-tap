e998fd4b9808a02eddcd77aa28efd527755b8ea2
Henk Bierlee <bierlee.henk@gmail.com>
2016-11-18 21:29:06 +0100

    Add example of sample data

---
+++ sample-data/sample-data-1.js
@@ -0,0 +1,44 @@
+{
+  "messages": [{
+    "to": "Piet",
+    "from": "Henk",
+    "content": "Hello Piet, this is Henk",
+    "timestamp": "1477937115000" 
+  }, {
+    "to": "Henk",
+    "from": "Piet",
+    "content": "Hi Henk, this is Piet!",
+    "timestamp": "1477937120000"
+  }, {
+    "to": "Piet",
+    "from": "Henk",
+    "content": "Bye Piet from Henk",
+    "timestamp": "1477937135000"
+  }, {
+    "to": "Henk",
+    "from": "Piet",
+    "content": "Bye bye Henk",
+    "timestamp": "1477937145000"
+  }, {
+    "to": "Jasper",
+    "from": "Henk",
+    "content": "Hi Jasper, this is Henk",
+    "timestamp": "1477930115000"
+  }, {
+    "to": "Henk",
+    "from": "Jasper",
+    "content": "Hi Henk, this is Jasper",
+    "timestamp": "1477931115000"
+  } ],
+  "users": {
+    "Henk": {
+      "userId": "Henk"
+    },
+    "Piet": {
+      "userId": "Piet"
+    },
+    "Jasper": {
+      "userId": "Jasper"
+    }
+  }
+}
